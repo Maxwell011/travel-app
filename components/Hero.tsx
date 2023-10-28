ceb06d03eb88a9cae408330f9b210bf1a5117892
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-10-28 21:35:55 +0100

    Completed Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 '>
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
       <div className='hero-map' />
 
       <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
@@ -15,10 +15,10 @@ const Hero = () => {
           className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]'
         />
         <h1 className='bold-52 lg:bold-88'>Putuk Truno Camp Area</h1>
-        <p className='regular-16 mt-6 text-gray-30 xl:max-w-[528px]'>
-          We want to on each of your journey seeking the satisfaction of seeing
-          the incorruptible beauty of nature. We can help you on an adventure
-          around the world in just one app.
+        <p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>
+          We want to be on each of your journeys seeking the satisfaction of
+          seeing the incorruptible beauty of nature. We can help you on an
+          adventure around the world in just one app
         </p>
 
         <div className='my-11 flex flex-wrap gap-5'>
@@ -37,8 +37,8 @@ const Hero = () => {
           </div>
 
           <p className='bold-16 lg:bold-20 text-blue-70'>
-            198k{" "}
-            <span className='regular-16 lg:regular-20 ml-1 '>
+            198k
+            <span className='regular-16 lg:regular-20 ml-1'>
               Excellent Reviews
             </span>
           </p>
@@ -48,25 +48,27 @@ const Hero = () => {
           <Button type='button' title='Download App' variant='btn_green' />
           <Button
             type='button'
-            title='How we Work?'
+            title='How we work?'
             icon='/play.svg'
-            variant='btn_green'
+            variant='btn_white_text'
           />
         </div>
       </div>
+
       <div className='relative flex flex-1 items-start'>
-        <div className='relative z-20 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
+        <div className='relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
           <div className='flex flex-col'>
             <div className='flexBetween'>
               <p className='regular-16 text-gray-20'>Location</p>
               <Image src='/close.svg' alt='close' width={24} height={24} />
             </div>
+            <p className='bold-20 text-white'>Aguas Calientes</p>
           </div>
-          <p className='bold-20 text-white'>Aguas Calientes</p>
+
           <div className='flexBetween'>
             <div className='flex flex-col'>
               <p className='regular-16 block text-gray-20'>Distance</p>
-              <p className='bold-20 text-white'>173.23 ml</p>
+              <p className='bold-20 text-white'>173.28 mi</p>
             </div>
             <div className='flex flex-col'>
               <p className='regular-16 block text-gray-20'>Elevation</p>
@@ -78,4 +80,5 @@ const Hero = () => {
     </section>
   );
 };
+
 export default Hero;
